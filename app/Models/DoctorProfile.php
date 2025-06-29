5cfa1122048e1e090d818d00b55524a2ccfd4ec3
anisoncloud <anisoncloud@gmail.com>
2025-06-30 00:08:25 +0600

    model migration added

--- app/Models/DoctorProfile.php
+++ app/Models/DoctorProfile.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DoctorProfile extends Model
 {
     //
+    public function Department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
--- database/migrations/2025_06_29_112655_create_doctor_profiles_table.php
+++ database/migrations/2025_06_29_112655_create_doctor_profiles_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
             $table->text('biography')->nullable();
             $table->string('profile_picture')->nullable();
             $table->string('short_description')->nullable();
-            $table->
             $table->timestamps();
         });
     }
