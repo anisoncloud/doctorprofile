bf40bb8ca13550c098a758dbb0497646d2e26383
anisoncloud <anisoncloud@gmail.com>
2025-06-29 18:18:01 +0600

    Initial commit

---
+++ database/migrations/2025_06_29_112655_create_doctor_profiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctor_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->int('department');
+            $table->string('phone')->nullable();
+            $table->string('email')->unique();
+            $table->text('biography')->nullable();
+            $table->string('profile_picture')->nullable();
+            $table->string('short_description')->nullable();
+            $table->
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctor_profiles');
+    }
+};
